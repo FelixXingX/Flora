0c28b754c705debc9b5f98042e22883a8373af56
FelixXingX <xingfelix@hotmail.com>
2022-08-21 01:56:41 -0400

    savedPlants

--- app/src/main/java/com/example/flora/databaseClicked.java
+++ app/src/main/java/com/example/flora/databaseClicked.java
@@ -3,23 +3,25 @@ package com.example.flora;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class databaseClicked extends AppCompatActivity {
 
     Button addMyPlant;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.database_clicked);
-
         String name = getIntent().getStringExtra("Plant Name");
         String type = getIntent().getStringExtra("Plant Type");
         String water = getIntent().getStringExtra("Plant Water");
+
+        String untilWater = getIntent().getStringExtra("Until Water");
 
         TextView nameTextView = findViewById(R.id.pName);
         TextView typeTextView = findViewById(R.id.pType);
@@ -34,7 +36,11 @@ public class databaseClicked extends AppCompatActivity {
         addMyPlant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(databaseClicked.this, myPlants.class));
+                Intent intent = new Intent(databaseClicked.this, myPlants.class);
+                intent.putExtra("Plant Name", name);
+                intent.putExtra("Plant Type", type);
+                intent.putExtra("Plant Water", untilWater);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/flora/myPlants.java
+++ app/src/main/java/com/example/flora/myPlants.java
@@ -4,11 +4,44 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class myPlants extends AppCompatActivity {
+
+    private ArrayList<Plant> savedPlantObjects = new ArrayList<Plant>(); // users personal plants
+    private static final String TAG = "MainActivity";
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_plants);
+
+        String name = getIntent().getStringExtra("Plant Name");
+        if (name != null) {
+            String type = getIntent().getStringExtra("Plant Type");
+            String water = getIntent().getStringExtra("Plant Water");
+            savedPlantObjects.add(new Plant (name,type,water));
+        }
+
+
+        if (savedPlantObjects.size() > 1) {
+            Collections.sort(savedPlantObjects, new Comparator<Plant>(){
+                public int compare (Plant o1, Plant o2) {
+                    int comp = o1.getDaysUntilWater() - o2.getDaysUntilWater();
+                    return comp;
+                }
+            });
+        }
+        RecyclerView recyclerView = findViewById(R.id.savedRecyclerView);
+        myPlantAdapter adapter = new myPlantAdapter(this, savedPlantObjects);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
     }
 }
